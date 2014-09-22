790306bfb53062e8f9d648424918dd8568374a21
ssloy <ssloy@castille.loria.fr>
2014-09-22 17:56:57 +0200

    file open

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "piece.h"
+
+using namespace std;
 int main(int argc, char** argv) {
-	if (2==argc) {
-		model = new Model(argv[1]);
-	} else {
-		model = new Model("pentomino.txt");
-	}
-	return 0;
+    string filename = string(2==argc ? argv[1] : "pentomino.txt");
+    ifstream in;
+    in.open(filename.c_str(), ifstream::in);
+    if (in.fail()) return 1;
+
+    vector<Piece> pieces;
+    while (!in.eof()) {
+        Piece p = Piece(in);
+        if (p.w() && p.h()) pieces.push_back(p);
+    }
+    in.close();
+
+    for (int i=0; i<(int)pieces.size(); i++) {
+        for (int j=0; j<i; j++) {
+            pieces[i].rotate();
+            if (j%3) pieces[i].flip();
+        }
+        pieces[i].print();
+        std::cout << std::endl;
+    }
+
+    pieces[3].print();
+    return 0;
 }
 
